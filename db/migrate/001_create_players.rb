01c32213f22424c2ffd2ffceb6b4b542d1197999
Peter Tseng <patseng@stanford.edu>
2012-10-28 16:00:04 -0700

    Create the player model

--- db/migrate/001_create_players.rb
+++ db/migrate/001_create_players.rb
@@ -1,8 +1,9 @@
 class CreatePlayers < ActiveRecord::Migration
   def change
     create_table :players do |t|
-      t.integer :player_number, default: 0
-      t.float :batting_average
+      t.string :player_id, :null => false
+      t.integer :batting_style, :default => 0 # left = -1, both = 0, right = 1
+      t.string :player_name
       t.timestamps
     end
   end
