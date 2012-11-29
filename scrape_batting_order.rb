98c56c6b4ca01295343f8c5c0dbae1e000cc95af
Nikhil Bhargava <nikhilb@stanford.edu>
2012-11-28 16:20:27 -0800

    Prepare to add roster order to games table

---
+++ scrape_batting_order.rb
@@ -0,0 +1,64 @@
+require 'rubygems'
+require 'active_record'
+require 'yaml'
+require 'CSV'
+require 'debugger'
+require 'date'
+
+require './models/game.rb'
+require './models/player.rb'
+require './models/performance.rb'
+
+require './teamMap.rb'
+
+dbconfig = YAML::load(File.open('database.yml'))
+ActiveRecord::Base.establish_connection(dbconfig)
+
+def parseGameLog(file_path)
+  # this opens the csv at the file path at iterates over the rows
+  CSV.foreach(file_path) do |row|
+    home_team = TeamMap.teamNamesToInt[row[6]]
+    
+    date = DateTime.parse(row[0])
+    number = row[1]
+
+    if number == 'A'
+      number = '1'
+    elsif number == 'B'
+      number = '2'
+    end
+
+    number = number.to_i
+
+    game = Game.find_by_home_team_and_game_date_and_game_number(home_team, date, number)
+
+    game.away_batting_spot_1 = row[105]
+    game.away_batting_spot_2 = row[108]
+    game.away_batting_spot_3 = row[111]
+    game.away_batting_spot_4 = row[114]
+    game.away_batting_spot_5 = row[117]
+    game.away_batting_spot_6 = row[120]
+    game.away_batting_spot_7 = row[123]
+    game.away_batting_spot_8 = row[126]
+    game.away_batting_spot_9 = row[129]
+    
+    game.home_batting_spot_1 = row[132]
+    game.home_batting_spot_2 = row[135]
+    game.home_batting_spot_3 = row[138]
+    game.home_batting_spot_4 = row[141]
+    game.home_batting_spot_5 = row[144]
+    game.home_batting_spot_6 = row[147]
+    game.home_batting_spot_7 = row[150]
+    game.home_batting_spot_8 = row[153]
+    game.home_batting_spot_9 = row[156]
+
+    game.save
+  end
+end
+
+# this iterates over every file/directory in raw_data (which we know is a directory)
+Dir.entries('./raw_data/game_logs').each do |entry|
+  if entry =~ /.*\.TXT/
+    parseGameLog("./raw_data/game_logs/#{entry}")
+  end
+end
